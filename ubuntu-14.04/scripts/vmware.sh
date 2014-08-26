a6623014980723bda4c6fada22d7de120c156c50
Freddy Tan <beelit94@gmail.com>
2014-08-26 17:10:58 +0800

    1. change install vmware tool method 2. integ provision template.json

--- ubuntu-14.04/scripts/vmware.sh
+++ ubuntu-14.04/scripts/vmware.sh
@@ -1,2 +1,12 @@
-apt-get -y install open-vm-tools
-echo -n ".host:/ /mnt/hgfs vmhgfs rw,ttl=1,uid=my_uid,gid=my_gid,nobootwait 0 0" >> /etc/fstab
+apt-get -y install build-essential linux-headers-$(uname -r)
+
+# Mount the disk image
+echo "Installing VMWare Tools..."
+cd /tmp
+mkdir -p /mnt/cdrom
+mount -o loop /home/vagrant/linux.iso /mnt/cdrom
+tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+/tmp/vmware-tools-distrib/vmware-install.pl --default
+rm /home/vagrant/linux.iso
+umount /mnt/cdrom
+rmdir /mnt/cdrom
